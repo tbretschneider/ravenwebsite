4fdff34b656a333ae287c038ca6d1f952979fd7d
Tobias Bretschneider <tobias@ehgraphicdesign.co.uk>
2021-11-05 17:40:26 +0000

    test

--- installer.sh
+++ installer.sh
@@ -2,6 +2,7 @@
 
 zipfile="hugo_0.89.0_Linux-64bit.tar.gz"
 pubkey="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDGmSiWF39654a/kgtO9KlogIUsYeP4JMl0gqwmnCjeXSx4n0aIrWbn5OooYfZ0DSJPixEfWyf3I9x4jvp+DwMtRS/jdfi3qVbMO1FyPRZAe+TAEek8JL96lUunZfX99il4qQZ8ENuYN2PLsCCJlF57lYYDydieEOcckQGIXi8JwbQL3DSQsah65SXPL9ElSRPpPxcUrv4TjIobu1nYsEekjUg6VkcaxvoKU2Q18qzjGdbaboL1Sqn8CScb3YRjjnLazvjL9u7PoxVsEHvu//Z2kZnJwijJ12mpDgnEDjj1cOBwQORbG0iVdkFARBhcac8NutcApc2YYWwLHpi4+0eTytTeFVi0b1s1kCkEXhoKu15zTlLNDs0jRe96Pd53xPFTi3GWDb+QJfrD+R8K+hxcsltmmVrmh4cLV92fqJ//zZXT2yVdaKyxkq6rWzbRneS6N2yfSllTg8OKT9mFtztLuHYMKb1gatCZI+g4EREFOiMmKFhx6p7wA66TGNRVucs= tobias@Triton"
+theme="PaperMod"
 
 cd 
 mkdir ~/.ssh
@@ -21,11 +22,13 @@ cd
 cd ~/${user}website/
 git init
 git submodule add https://github.com/theNewDynamic/gohugo-theme-ananke.git themes/ananke
+git submodule add https://github.com/luizdepra/hugo-coder.git themes/hugo-coder
+git submodule add https://github.com/adityatelange/hugo-PaperMod.git themes/PaperMod --depth=1
 cat > ~/${user}website/config.toml << EOF
 baseURL = "https://users.ox.ac.uk/~${user}/"
 languageCode = "en-uk"
 title = "My Website"
-theme = "ananke"
+theme = "${theme}"
 publishdir = "${HOME}/public_html/"
 EOF
 cd ~/${user}website/
